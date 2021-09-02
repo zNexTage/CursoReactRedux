e9e64f1254637d9a07f70bee1eddee599f6d048c
christian_oliveira <christianoliveira1908@hotmail.com>
2021-09-02 20:32:11 -0300

    Realização do projeto cadastro utilizando next.js

---
+++ projeto-cadastro/src/pages/index.jsx
@@ -0,0 +1,12 @@
+const Home = () => (
+    <div>
+        <h1>
+            Fundamentos de Next.js & React
+        </h1>
+        <h2>
+            Vamos estudar esse framework
+        </h2>
+    </div>
+)
+
+export default Home;
