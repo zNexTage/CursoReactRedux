a8602d6870d0eae7a3b9f623a3d39e04d7f4258b
christian_oliveira <christianoliveira1908@hotmail.com>
2021-09-20 21:58:50 -0300

    Criação do projeto Cadastro

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600`}>
+      <span className={`text-4xl`}>
+        Texto
+      </span>
+    </div>
+  )
+}
